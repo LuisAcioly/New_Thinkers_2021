1bc03103140cf07940d059e9c30630d4fba03d19
LuisBastos <38045709+TropZZ@users.noreply.github.com>
2021-04-13 15:53:18 -0300

    Arquitetura Hexagonal

---
+++ Trilha DotNET/NewThinkersProject/NewThinkersProject/DTO/Pokemon/AddPokemon/AddPokemonResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewThinkersProject.DTO.Pokemon.AddPokemon
+{
+    public class AddPokemonResponse
+    {
+        public int id { get; set; }
+
+        public string message { get; set; }
+    }
+}
---
+++ Trilha DotNET/NewThinkersProject/NewThinkersProject/DTO/Pokemon/GetPokemonList/GetPokemonListResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using NewThinkersProject.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewThinkersProject.DTO.Pokemon.DeletePokemon
+{
+    public class GetPokemonListResponse
+    {
+        public List<Entities.Pokemon> list { get; set; }
+        public string message { get; set; }
+    }
+}
